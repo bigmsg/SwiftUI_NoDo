e8768f41aa3f753b3319ce4978afa2ad23344adb
bigmsg <travelcar@naver.com>
2019-11-11 17:34:18 +0900

    Cell 작성해보기

--- SwiftUI_NoDo/ContentView.swift
+++ SwiftUI_NoDo/ContentView.swift
@@ -9,8 +9,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var nodo: String = ""
+    @State var nodoList = [String]()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                HStack(spacing: 5) {
+                    
+                    Image(systemName: "plus.circle")
+                        .padding(.leading)
+                    
+                    Group {
+                        TextField("What Will You Not Do Today?", text: self.$nodo, onEditingChanged: { (changed) in
+                            print(changed)
+                        }) {
+                            //print("onCommit")
+                            self.nodoList.insert(self.nodo, at: 0)
+                            print("Added item: \(self.nodoList)")
+                            self.nodo = ""
+                        }.padding(.all, 12)
+                    }.background(Color.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .shadow(radius: 5)
+                    .padding(.trailing, 8)
+                }
+                
+                
+                List (nodoList, id: \.self) { item in
+                    Cell(title: item)
+                }
+                
+            }.navigationBarTitle(Text("NoDo"))
+            
+            
+            
+        }
+    }
+}
+
+struct Cell: View {
+    var title: String = ""
+    var isChecked: Bool = false
+    var addTime: String = ""
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack (alignment: .top) {
+                Text(self.title)
+                Spacer()
+                Button(action: {
+                    print("check click")
+                }) {
+                    Image(systemName: "plus.circle")
+                }
+            }.frame(maxHeight: .infinity).background(Color.red)
+            HStack {
+                Spacer()
+                Text("Add Just now")
+            }
+        }.background(Color.gray).frame(height: 100)
+        .cornerRadius(5)
     }
 }
 
